f24869cb152b72e7537ac80aa98c7cbf78b2a8d5
Sergio <89666307+feelfreetofee@users.noreply.github.com>
2025-07-22 02:56:07 +0200

    fix(tts): Update Token Key
    under LocalStorage

--- tts/index.js
+++ tts/index.js
@@ -23,7 +23,7 @@ import * as OAuth2 from './twitch/oauth2'
 if (!client.token)
     try {
         if (client.token = OAuth2.response())
-            localStorage.setItem('twitch_token', client.token)
+            localStorage.setItem('tts_twitch_token', client.token)
         else
             location.replace(
                 OAuth2.authorize(
