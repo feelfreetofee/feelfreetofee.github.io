f50d4707ff2c6b86cde501b4be6fc8dded3314ef
Lorenc <snellemany@gmail.com>
2025-12-22 01:35:40 +0100

    Add audio playback and URL masking features
    Added functionality to play a specific audio when the keyword 'pedro' is detected in chat messages. Also implemented URL masking in messages.

--- tts/index.js
+++ tts/index.js
@@ -1,10 +1,12 @@
 import {client} from './client'
-
 import {tts, TTS} from './tts'
 
 const languageDetector = await LanguageDetector.create({
     expectedInputLanguages: ['en-US', 'es-ES'],
 })
+
+const pedroAudio = new Audio('https://www.myinstants.com/media/sounds/pedro-song.mp3')
+pedroAudio.volume = 0.8
 
 function detectLanguage(text) {
     return languageDetector.detect(text)
@@ -12,10 +14,35 @@ function detectLanguage(text) {
             languageDetector.expectedInputLanguages.includes(detectedLanguage)))
 }
 
+function playPedroSong() {
+    pedroAudio.currentTime = 0
+    pedroAudio.play()
+    
+    setTimeout(() => {
+        pedroAudio.pause()
+        pedroAudio.currentTime = 0
+    }, 15000)
+}
+
+function processMessageText(text) {
+    if (/pedro/i.test(text)) {
+        playPedroSong()
+        return null
+    }
+    
+    const urlRegex = /(https?:\/\/[^\s]+)|(www\.[^\s]+)|([a-zA-Z0-9-]+\.(com|net|org|io|tv|gg|xyz|me|co)[^\s]*)/gi
+    return text.replace(urlRegex, 'LINK')
+}
+
 client.addEventListener('notification', ({data}) => {
     if (data.metadata.subscription_type !== 'channel.chat.message') return
     const {text} = data.payload.event.message
-    detectLanguage(text).then(r => TTS(text, r?.detectedLanguage))
+    
+    const processedText = processMessageText(text)
+    
+    if (processedText) {
+        detectLanguage(processedText).then(r => TTS(processedText, r?.detectedLanguage))
+    }
 })
 
 import * as OAuth2 from './twitch/oauth2'
@@ -36,6 +63,7 @@ if (!client.token)
         // TODO UI
         console.error(`${error.name} - ${error.message}`)
     }
+
 if (client.token)
     OAuth2.validate(client)
     .then(validation => client.connect(client.user_id = validation.user_id))
